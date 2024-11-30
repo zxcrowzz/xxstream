c35ec7ffacd402d7ba2b4a83ef8a453d025e5266
zxcrowzz <pantsbro4@gmail.com>
2024-11-29 16:38:30 -0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
- 
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   isVerified: { type: Boolean, default: false },
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  profileImageUrl: { type: String }
+  profileImageUrl: { type: String },
   
+  // TOS agreement details
+  tosAcceptedAt: { type: Date, required: true },  // Timestamp when user agreed to the TOS
+  tosVersion: { type: String, default: '1.0' },    // Version of the TOS user accepted
 });
 
 module.exports = mongoose.model('User', userSchema);
